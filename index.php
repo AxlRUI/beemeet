98371e46cc23c6c7dece0686fbcc96b30840b4a3
AxlRUI <axelpforestrubio@hotmail.com>
2017-07-04 15:08:10 -0500

    s

--- index.php
+++ index.php
@@ -8,24 +8,7 @@
 </head>
 <body class="fondo1">
 	<section class="contenido">
-		<section class="contenedor">
-			<div>
-				<h1 class="honey cita">BeeMeet</h1>
-				<h3>Es una aplicación de organización orientada a proyectos y trabajos en equipo. Puedes hacer una cuenta de manera totalmente gratuita</h3>
-			</div>
-			<div>
-				<form action="">
-					<div>
-						<label for="user">Usuario</label>
-						<input type="text" name="user" required="">
-					</div>
-					<div>
-						<label for="pass">Contraseña</label>
-						<input type="password" name="pass" required="">
-					</div>
-				</form>
-			</div>
-		</section>
+		s
 	</section>
 </body>
 </html>
